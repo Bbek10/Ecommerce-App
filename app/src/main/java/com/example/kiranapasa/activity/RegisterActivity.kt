18ade5e847a8f31c2dd9d6f20510fbdd8a0c888f
Bbek10 <bibekmanandhar2@gmail.com>
2022-12-07 22:34:20 +0545

    first commit

---
+++ app/src/main/java/com/example/kiranapasa/activity/RegisterActivity.kt
@@ -0,0 +1,80 @@
+package com.example.kiranapasa.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.kiranapasa.databinding.ActivityRegisterBinding
+import com.example.kiranapasa.model.UserModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    //open Login from Register activity
+        binding.button4.setOnClickListener{
+
+            openLogin()
+        }
+
+        //for login button press
+        binding.button3.setOnClickListener {
+            validateUser()
+        }
+
+    }
+
+    private fun validateUser() {
+        if(binding.userName.text!!.isEmpty() || binding.userNumber.text!!.isEmpty())
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+        else
+            storeData()
+
+    }
+
+    private fun storeData() {
+        //to show messages while storing
+        val builder = AlertDialog.Builder(this).setTitle("Loading....")
+            .setMessage("Please Wait")
+            .setCancelable(false)
+            .create()
+        builder.show()
+
+        val preferences= this.getSharedPreferences("user", MODE_PRIVATE)
+        val editor = preferences.edit()
+
+        editor.putString("number",binding.userNumber.text.toString())
+        editor.putString("name", binding.userNumber.text.toString())
+        editor.apply()
+
+        //to store data into firebase
+
+        val data = UserModel(userName =  binding.userName.text.toString(), userPhoneNumber = binding.userNumber.text.toString() )
+        Firebase.firestore.collection("users").document(binding.userNumber.text.toString())
+            .set(data).addOnSuccessListener {
+                Toast.makeText(this,"User Registered.",Toast.LENGTH_SHORT).show()
+                builder.dismiss()
+                openLogin()
+
+            }
+            .addOnFailureListener{
+                builder.dismiss()
+
+                Toast.makeText(this,"Something went wrong.",Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun openLogin() {
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
+
+
+}
---
+++ app/src/main/java/com/example/kiranapasa/model/UserModel.kt
@@ -0,0 +1,11 @@
+package com.example.kiranapasa.model
+
+data class UserModel(
+    val userName: String?= "",
+    val userPhoneNumber: String?="",
+    val street :String?="",
+    val state :String?="",
+    val city :String?="",
+    val pinCode :String?="",
+
+    )
